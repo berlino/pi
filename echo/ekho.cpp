802072cf9f50f80116255a92e0e1e5393be91088
Berlin <berlin993428@gmail.com>
2014-07-06 16:59:01 -0500

    echo
    makefile and cpp

---
+++ echo/ekho.cpp
@@ -0,0 +1,15 @@
+#include "config.h" // this file is generated when building Ekho
+#include "ekho.h"
+
+using namespace ekho;
+
+int main(int argc, char **argv)
+{
+
+
+  Ekho wong("Mandarin");
+
+  wong.blockSpeak("ÄãºÃ£¬»¶Ó­£¡");
+
+  return 0;
+}
