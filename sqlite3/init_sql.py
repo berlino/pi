e59cbb7527c08e814ef4f572b09011547ff02321
berlino <berlin993428@gmail.com>
2014-07-11 22:34:01 +0800

    sql

---
+++ sqlite3/init_sql.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+conn=sqlite3.connect('hello.db')
+
+c=conn.cursor()
+c.execute('''create table entries (block integer not null,state integer,parking integer,park_num integer,'text' text )''')
+conn.commit()
+
+for i in range(1,21):
+	if i==5:
+		c.execute("insert into entries (block,state,parking,park_num) values (?,?,?,?)",[i,0,65,255])
+	elif i==15:
+		c.execute("insert into entries (block,state,parking,park_num) values (?,?,?,?)",[i,0,64,255])
+	else:
+		c.execute("insert into entries (block,state,parking,park_num) values (?,?,?,?)" ,[i,0,0,0])
+
+	conn.commit()
+
+cur=c.execute("select * from entries")
+print cur.fetchall()
